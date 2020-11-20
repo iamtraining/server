94534ffa791ed3a55d24a532174f356634cdc978
iamtraining <pinkpony1952@yandex.ru>
2020-11-21 01:08:33 +0300

    byte index 2 is out of bounds of c, src main.rs:233:22

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fmt::{Display, Formatter, Debug};
 use std::fmt::Result as FmtResult;
 use std::str;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 fn main() {
     let server = Server::new("127.0.0.1:3000".to_string());
@@ -35,7 +36,9 @@ impl Server {
                             println!("request received: \n{}", String::from_utf8_lossy(&buf));
                             //Request::try_from(&buf as &[u8]);
                             match Request::try_from(&buf[..]) {
-                                Ok(request) => {},
+                                Ok(request) => {
+                                    dbg!(request);
+                                },
                                 Err(err) => println!("cant parse a request")
                             }
                         },
@@ -49,6 +52,7 @@ impl Server {
     }
 }
 
+#[derive(Debug)]
 enum HTTPMethod {
     GET,
     HEAD,
@@ -66,7 +70,6 @@ impl FromStr for HTTPMethod {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            _ => Err(MethodError),
             "GET" => Ok(Self::GET),
             "HEAD" => Ok(Self::HEAD),
             "POST" => Ok(Self::POST),
@@ -76,6 +79,7 @@ impl FromStr for HTTPMethod {
             "OPTIONS" => Ok(Self::OPTIONS),
             "TRACE" => Ok(Self::TRACE),
             "PATCH" => Ok(Self::PATCH),
+            _ => Err(MethodError),
         }
     }
 }
@@ -88,22 +92,23 @@ impl From<MethodError> for RequestError {
     }
 }
 
-struct Request {
+#[derive(Debug)]
+struct Request<'buf> {
     method: HTTPMethod,
-    path:  &str,
-    query_string: Option<&str>,
+    path:  &'buf str,
+    query_string: Option<QueryString<'buf>>,
 }
 
-impl Request {
+impl<'buf> Request<'buf> {
     fn from_byte_to_struct(buf: &[u8]) -> Result<Self, String> {
         unimplemented!()
     }
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = RequestError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         /*
         match str::from_utf8(&buf) {
             Ok(request) => {},
@@ -141,7 +146,7 @@ impl TryFrom<&[u8]> for Request {
         let mut q_str = None;
 
         if let Some(i) = path.find('?') {
-            q_str = Some(&path[i+1..]);
+            q_str = Some(QueryString::from((&path[i+1..]))); 
             path = &path[..i];
         }
 
@@ -194,4 +199,52 @@ fn next_word(str: &str) -> Option<(&str, &str)> {
     }
     
     None
+}
+
+#[derive(Debug)]
+
+struct QueryString<'buf> {
+    params: HashMap<&'buf str, Value<'buf>>
+}
+
+#[derive(Debug)]
+
+enum Value<'buf> {
+    Single(&'buf str),
+    Multiple(Vec<&'buf str>),
+}
+
+impl <'buf> QueryString<'buf> {
+    fn parse(&self, key:&str) -> Option<&Value> {
+        self.params.get(key)
+    }
+}
+
+// a=1&b=2&c&d=&e===&d=7&d=abc
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(s: &'buf str) -> Self {
+        let mut map = HashMap::new();
+
+        for s_s in s.split('&') {
+            let mut k = s_s;
+            let mut v = "";
+            if let Some(i) = s.find('=') {
+                k = &s_s[..i];
+                v = &s_s[i+1..];
+            }
+
+            map.entry(k)
+            .and_modify(|cur: &mut Value| match cur {
+                Value::Single(prev) => {
+                    *cur = Value::Multiple(vec![prev, v])
+                },
+                Value::Multiple(vec) => vec.push(v)
+            })
+            .or_insert(Value::Single(v));
+        }
+
+        QueryString {
+            params:map,
+        }
+    }
 }
